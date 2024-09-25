c721f1c971cbf39c1ea867afe58187ddff852e6f
Danil Parkhomenko <141407786+parap3t@users.noreply.github.com>
2024-09-25 17:33:01 +0500

    Update models.py

--- code/database/models.py
+++ code/database/models.py
@@ -1,47 +1,58 @@
-# Импортируем необходимые модули
-from sqlalchemy import String,Integer
-from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
-from sqlalchemy.ext.asyncio import AsyncAttrs,async_sessionmaker,create_async_engine 
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-# Создём бд и подключаемся к ней
+# Создаём бд и подключаемся к ней
 engine = create_async_engine(url="sqlite+aiosqlite:///db.sqlite3")
 async_session = async_sessionmaker(engine)
 
 # Создаём родительский класс для построения моделей
-class Base(AsyncAttrs,DeclarativeBase):
+class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 # Таблица с забаненными пользователями
 class BannedUser(Base):
     __tablename__ = "banned_users"
 
-    id : Mapped[int] = mapped_column(primary_key=True)
-    chat_id : Mapped[int] = mapped_column(Integer)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    chat_id: Mapped[int] = mapped_column(Integer)
 
-# Таблица с пользователями,которые находятся в рассылке
+# Таблица с пользователями, которые находятся в рассылке
 class UserInMailing(Base):
     __tablename__ = "users_in_mailing"
 
-    id : Mapped[int] = mapped_column(primary_key=True)
-    chat_id : Mapped[int] = mapped_column(Integer)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    chat_id: Mapped[int] = mapped_column(Integer)
 
 # Таблица с айди чата администраторов
 class Admin(Base):
-    __tablename__ = "Admins"
+    __tablename__ = "admins"
 
-    id : Mapped[int] = mapped_column(primary_key=True)
-    chat_id : Mapped[int] = mapped_column(Integer)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    chat_id: Mapped[int] = mapped_column(Integer)
 
 # Таблица для мероприятий
 class Event(Base):
-    __tablename__ = "Events"
+    __tablename__ = "events"
 
-    id : Mapped[int] = mapped_column(primary_key=True)
-    name : Mapped[str] = mapped_column(String(60))
-    description : Mapped[str] = mapped_column(String(150))
-    foreign_key : Mapped[str] = mapped_column(String(5))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(60))
+    date: Mapped[str] = mapped_column(String(14))
+    description: Mapped[str] = mapped_column(String(150))
+    is_signup_open: Mapped[int] = mapped_column(Integer)
+
+# Таблица для записавшихся на меропрития пользователей
+class EventSingUp(Base):
+    __tablename__ = "event_singup"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(100))
+    phone: Mapped[int] = mapped_column(Integer)
+    chat_id: Mapped[int] = mapped_column(Integer)
+    event_status: Mapped[int] = mapped_column(Integer)
+    event_id: Mapped[int] = mapped_column(ForeignKey(Event.id))
 
 # При запуске главного файла создаём таблицы
-async def async_main() -> None:
+async def async_main():
     async with engine.begin() as conn:
       await conn.run_sync(Base.metadata.create_all)
