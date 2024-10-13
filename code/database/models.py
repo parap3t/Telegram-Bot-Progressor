185641a4423c154e2966a9a9163588a1ee2b7849
chulkov-alex <chulkov-alex@yandex-team.ru>
2024-10-14 02:45:43 +0300

    change db file source

--- code/database/models.py
+++ code/database/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import String, Integer, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-from code.config import DB_FILE
+from config import DB_FILE
 
 # Создаём бд и подключаемся к ней
 engine = create_async_engine(url="sqlite+aiosqlite:///" + DB_FILE)
